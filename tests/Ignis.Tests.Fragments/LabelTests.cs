97bba37145a0938e9cd940b5b810390a1f632401
dvolper <github@david.vollmers.org>
2023-07-13 09:30:59 +0200

    label tests

--- tests/Ignis.Tests.Fragments/LabelTests.cs
+++ tests/Ignis.Tests.Fragments/LabelTests.cs
@@ -3,7 +3,7 @@
 public class LabelTests : TestContext
 {
     [Fact]
-    public void Label_PropertyInfo()
+    public void Label_PropertyInfo_Default()
     {
         var propertyInfo = typeof(TestModel).GetProperty(nameof(TestModel.StringProperty))!;
 
@@ -12,5 +12,65 @@ public class LabelTests : TestContext
         var label = fragment.Find("label");
         Assert.Equal(nameof(TestModel.StringProperty), label.GetAttribute("for"));
         Assert.Equal(nameof(TestModel.StringProperty), label.TextContent);
+    }
+
+    [Fact]
+    public void Label_PropertyInfo_Default_WithDisplayNameAttribute()
+    {
+        var propertyInfo = typeof(TestModel).GetProperty(nameof(TestModel.StringPropertyWithDisplayNameAttribute))!;
+
+        var fragment = Render(Label(propertyInfo)!);
+
+        var label = fragment.Find("label");
+        Assert.Equal(nameof(TestModel.StringPropertyWithDisplayNameAttribute), label.GetAttribute("for"));
+        Assert.Equal(TestModel.StringPropertyWithDisplayNameAttributeValue, label.TextContent);
+    }
+
+    [Fact]
+    public void Label_PropertyInfo_Default_WithDisplayAttribute()
+    {
+        var propertyInfo = typeof(TestModel).GetProperty(nameof(TestModel.StringPropertyWithDisplayAttribute))!;
+
+        var fragment = Render(Label(propertyInfo)!);
+
+        var label = fragment.Find("label");
+        Assert.Equal(nameof(TestModel.StringPropertyWithDisplayAttribute), label.GetAttribute("for"));
+        Assert.Equal(TestModel.StringPropertyWithDisplayAttributeValue, label.TextContent);
+    }
+    
+    [Fact]
+    public void Label_Expression_Default()
+    {
+        var model = new TestModel();
+        
+        var fragment = Render(Label(() => model.StringProperty)!);
+
+        var label = fragment.Find("label");
+        Assert.Equal(nameof(TestModel.StringProperty), label.GetAttribute("for"));
+        Assert.Equal(nameof(TestModel.StringProperty), label.TextContent);
+    }
+
+    [Fact]
+    public void Label_Expression_Default_WithDisplayNameAttribute()
+    {
+        var model = new TestModel();
+        
+        var fragment = Render(Label(() => model.StringPropertyWithDisplayNameAttribute)!);
+
+        var label = fragment.Find("label");
+        Assert.Equal(nameof(TestModel.StringPropertyWithDisplayNameAttribute), label.GetAttribute("for"));
+        Assert.Equal(TestModel.StringPropertyWithDisplayNameAttributeValue, label.TextContent);
+    }
+
+    [Fact]
+    public void Label_Expression_Default_WithDisplayAttribute()
+    {
+        var model = new TestModel();
+        
+        var fragment = Render(Label(() => model.StringPropertyWithDisplayAttribute)!);
+
+        var label = fragment.Find("label");
+        Assert.Equal(nameof(TestModel.StringPropertyWithDisplayAttribute), label.GetAttribute("for"));
+        Assert.Equal(TestModel.StringPropertyWithDisplayAttributeValue, label.TextContent);
     }
 }
--- tests/Ignis.Tests.Fragments/TestModel.cs
+++ tests/Ignis.Tests.Fragments/TestModel.cs
@@ -1,6 +1,21 @@
-﻿namespace Ignis.Tests.Fragments;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ignis.Tests.Fragments;
 
 public class TestModel
 {
-    public string StringProperty { get; set; }
+    public const string StringPropertyWithDisplayNameAttributeValue =
+        "String Property With DisplayName Attribute Value";
+
+    public const string StringPropertyWithDisplayAttributeValue =
+        "String Property With Display Attribute Value";
+
+    public string? StringProperty { get; set; }
+
+    [DisplayName(StringPropertyWithDisplayNameAttributeValue)]
+    public string? StringPropertyWithDisplayNameAttribute { get; set; }
+
+    [Display(Name = StringPropertyWithDisplayAttributeValue)]
+    public string? StringPropertyWithDisplayAttribute { get; set; }
 }
