de977bbbc7bef0a0fccf3368205b51392be83a77
dvolper <github@david.vollmers.org>
2023-07-13 09:50:53 +0200

    tests

---
+++ tests/Ignis.Tests.Fragments/InputTests.cs
@@ -0,0 +1,73 @@
+﻿namespace Ignis.Tests.Fragments;
+
+public class InputTests : TestContext
+{
+    [Fact]
+    public void Input_Value_OnInput()
+    {
+        const string newValue = "New Test Value";
+        var value = "Test Value";
+
+        var fragment = Render(Input(value, s => value = s)!);
+
+        var input = fragment.Find("input");
+        Assert.Equal(value, input.GetAttribute("value"));
+
+        input.Change(newValue);
+        // We cannot check the input attribute again since there is no render update...
+        Assert.Equal(newValue, value);
+    }
+    
+    [Fact]
+    public void Input_Value_OnInput_Exception()
+    {
+        const string exceptionMessage = "Exception Message";
+        const string value = "Test Value";
+        const string newValue = "New Test Value";
+
+        var fragment = Render(Input(value, s => throw new Exception(exceptionMessage))!);
+
+        var input = fragment.Find("input");
+        Assert.Equal(value, input.GetAttribute("value"));
+
+        input.Change(newValue);
+        Assert.Equal(value, input.GetAttribute("value"));
+    }
+
+    [Fact]
+    public void Input_Value_OnInputAsync_Exception()
+    {
+        const string exceptionMessage = "Exception Message";
+        const string value = "Test Value";
+        const string newValue = "New Test Value";
+
+#pragma warning disable CS1998
+        var fragment = Render(Input(value, async s => throw new Exception(exceptionMessage))!);
+#pragma warning restore CS1998
+
+        var input = fragment.Find("input");
+        Assert.Equal(value, input.GetAttribute("value"));
+
+        input.Change(newValue);
+        Assert.Equal(value, input.GetAttribute("value"));
+    }
+    
+    [Fact]
+    public void Input_Expression()
+    {
+        const string newValue = "New Test Value";
+        const string value = "Test Value";
+        var model = new TestModel
+        {
+            StringProperty = value
+        };
+
+        var fragment = Render(Input(() => model.StringProperty)!);
+
+        var input = fragment.Find("input");
+        Assert.Equal(value, input.GetAttribute("value"));
+
+        input.Change(newValue);
+        Assert.Equal(newValue, model.StringProperty);
+    }
+}
