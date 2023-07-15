fff095fd52088d896d03ae895b6e2e717ee46a9b
dvolper <github@david.vollmers.org>
2023-07-15 11:47:16 +0200

    disclosure concept

--- packages/Tailwind/Ignis.Components.HeadlessUI/Disclosure.cs
+++ packages/Tailwind/Ignis.Components.HeadlessUI/Disclosure.cs
@@ -5,16 +5,47 @@ namespace Ignis.Components.HeadlessUI;
 
 public sealed class Disclosure : IgnisComponentBase, IDynamicComponent
 {
+    public bool IsOpen { get; private set; }
+    
     [Parameter] public string? AsElement { get; set; }
 
     [Parameter] public Type? AsComponent { get; set; } = typeof(Fragment);
 
+    [Parameter] public RenderFragment<Disclosure>? ChildContent { get; set; }
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object?>? Attributes { get; set; }
+
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
         builder.OpenAs(0, this);
+        builder.AddMultipleAttributes(1, Attributes!);
         
-        
+        builder.OpenComponent<CascadingValue<Disclosure>>(2);
+        builder.AddAttribute(3, nameof(CascadingValue<Disclosure>.IsFixed), true);
+        builder.AddAttribute(4, nameof(CascadingValue<Disclosure>.Value), this);
+        builder.AddAttribute(5, nameof(CascadingValue<Disclosure>.ChildContent), ChildContent?.Invoke(this));
+
+        builder.CloseComponent();
         
         builder.CloseAs(this);
+    }
+
+    public void Open()
+    {
+        if (IsOpen) return;
+        
+        IsOpen = true;
+        
+        ForceUpdate();
+    }
+
+    public void Close()
+    {
+        if (!IsOpen) return;
+        
+        IsOpen = false;
+        
+        ForceUpdate();
     }
 }
