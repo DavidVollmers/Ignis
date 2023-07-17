c6eed6b125a4741b95f5baaa921d12be1a772bca
dvol <github@david.vollmers.org>
2023-07-17 09:09:11 +0200

    contributing guide for Ignis.Website

---
+++ Ignis.Website/CONTRIBUTING.md
@@ -0,0 +1,9 @@
+# Setup
+
+Make sure to install all NPM dependencies in the repository by running the following command in the root folder:
+
+```shell
+npm i
+```
+
+Afterwards you can start the `Ignis.Website` launch profile.
--- packages/Ignis.Components.Web/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const components_1 = require("@ignis.net/components");
-const scroll_detector_1 = require("./scroll-detector");
-(0, components_1.registerComponent)('Ignis.Components.Web.ScrollDetector', scroll_detector_1.ScrollDetector);
