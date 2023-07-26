e0927a46056380f55c3f3fcf168a28978972c35a
dvolper <github@david.vollmers.org>
2023-07-26 13:56:12 +0200

    web assembly page service implementation

--- website/Ignis.Website.WebAssembly/Program.cs
+++ website/Ignis.Website.WebAssembly/Program.cs
@@ -3,12 +3,13 @@ using Ignis.Website;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Ignis.Website.WebAssembly;
+using Ignis.Website.WebAssembly.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddIgnisWebsite();
+builder.Services.AddIgnisWebsite<PageService>();
 builder.Services.AddIgnisWebAssembly();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
---
+++ website/Ignis.Website.WebAssembly/Services/PageService.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Ignis.Markdown.Processor.Contracts;
+using Ignis.Website.Services;
+
+namespace Ignis.Website.WebAssembly.Services;
+
+internal class PageService : PageServiceBase
+{
+    private const string DocsPath = "/wwwroot/docs";
+
+    private readonly HttpClient _httpClient;
+    private readonly ILogger<PageService> _logger;
+
+    public PageService(HttpClient httpClient, ILogger<PageService> logger)
+    {
+        _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    protected override async Task<string?> LoadPageContentAsync(Page page, CancellationToken cancellationToken)
+    {
+        var path = DocsPath + page.Link;
+        if (path == "/") path += "index";
+        path += ".html";
+
+        try
+        {
+            return await _httpClient.GetStringAsync(path, cancellationToken);
+        }
+        catch (HttpRequestException exception)
+        {
+            if (exception.StatusCode != HttpStatusCode.NotFound) throw;
+
+            _logger.LogWarning("Sitemap file not found.");
+            return null;
+        }
+    }
+
+    protected override Task<Section[]?> LoadSectionsAsync(CancellationToken cancellationToken)
+    {
+        const string path = DocsPath + "/sitemap.json";
+
+        return _httpClient.GetFromJsonAsync<Section[]>(path, cancellationToken);
+    }
+}
