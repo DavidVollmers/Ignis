36719cd4a2b997fb786b9ab5add49b0d56f611e5
dvolper <github@david.vollmers.org>
2023-07-27 18:03:29 +0200

    disclosure panel

--- packages/Tailwind/Ignis.Components.HeadlessUI/DisclosurePanel.cs
+++ packages/Tailwind/Ignis.Components.HeadlessUI/DisclosurePanel.cs
@@ -1,11 +1,12 @@
-﻿using Ignis.Components.Web;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 
 namespace Ignis.Components.HeadlessUI;
 
-public sealed class DialogPanel : IgnisRigidComponentBase, IDynamicParentComponent
+public sealed class DisclosurePanel : IgnisRigidComponentBase, IDisclosurePanel
 {
+    private readonly AttributeCollection _attributes;
+
     private Type? _asComponent;
     private string? _asElement;
 
@@ -33,16 +34,24 @@ public sealed class DialogPanel : IgnisRigidComponentBase, IDynamicParentCompone
         }
     }
 
-    [CascadingParameter] public IDialog Dialog { get; set; } = null!;
+    /// <inheritdoc />
+    [Parameter]
+    public string? Id { get; set; }
+
+    [CascadingParameter] public IDisclosure Disclosure { get; set; } = null!;
 
     /// <inheritdoc />
     [Parameter]
-    public RenderFragment<IDynamicComponent>? _ { get; set; }
+    public RenderFragment<IDisclosurePanel>? _ { get; set; }
 
     [Parameter] public RenderFragment? ChildContent { get; set; }
 
     [Parameter(CaptureUnmatchedValues = true)]
-    public IEnumerable<KeyValuePair<string, object?>>? AdditionalAttributes { get; set; }
+    public IEnumerable<KeyValuePair<string, object?>>? AdditionalAttributes
+    {
+        get => _attributes.AdditionalAttributes;
+        set => _attributes.AdditionalAttributes = value;
+    }
 
     /// <inheritdoc />
     public ElementReference? Element { get; set; }
@@ -51,21 +60,28 @@ public sealed class DialogPanel : IgnisRigidComponentBase, IDynamicParentCompone
     public object? Component { get; set; }
 
     /// <inheritdoc />
-    public IEnumerable<KeyValuePair<string, object?>>? Attributes => AdditionalAttributes;
+    public IEnumerable<KeyValuePair<string, object?>> Attributes => _attributes;
 
-    public DialogPanel()
+    public DisclosurePanel()
     {
         AsElement = "div";
+
+        _attributes = new AttributeCollection(new[]
+        {
+            () => new KeyValuePair<string, object?>("id", Id ?? Disclosure.Id + "-panel")
+        });
     }
 
     /// <inheritdoc />
     protected override void OnRender()
     {
-        if (Dialog == null)
+        if (Disclosure == null)
         {
             throw new InvalidOperationException(
-                $"{nameof(DialogPanel)} must be used inside a {nameof(HeadlessUI.Dialog)}.");
+                $"{nameof(DisclosurePanel)} must be used inside a {nameof(HeadlessUI.Disclosure)}.");
         }
+
+        Disclosure.SetPanel(this);
     }
 
     /// <inheritdoc />
@@ -73,18 +89,7 @@ public sealed class DialogPanel : IgnisRigidComponentBase, IDynamicParentCompone
     {
         builder.OpenAs(0, this);
         builder.AddMultipleAttributes(1, Attributes!);
-        // ReSharper disable once VariableHidesOuterVariable
-        builder.AddContentFor(2, this, builder =>
-        {
-            builder.OpenComponent<FocusDetector>(3);
-            builder.AddAttribute(4, nameof(FocusDetector.Id), Dialog.Id);
-            builder.AddAttribute(5, nameof(FocusDetector.OnBlur), EventCallback.Factory.Create(this, () => Dialog.Close()));
-            // ReSharper disable once VariableHidesOuterVariable
-            builder.AddAttribute(6, nameof(FocusDetector.ChildContent),
-                this.GetChildContent<IDynamicComponent, DialogPanel>(ChildContent));
-
-            builder.CloseComponent();
-        });
+        builder.AddChildContentFor<IDisclosurePanel, DisclosurePanel>(2, this, ChildContent);
 
         builder.CloseAs(this);
     }
