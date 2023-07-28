981a55112fce037fcdd7e341b914c1946307893e
dvolper <github@david.vollmers.org>
2023-07-28 11:33:04 +0200

    restructure website to only use blazor pages

--- website/Ignis.Website/IgnisWebsiteExtensions.cs
+++ website/Ignis.Website/IgnisWebsiteExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Ignis.Components;
+﻿using System.Reflection;
+using Ignis.Components;
 using Ignis.Components.Web;
 using Ignis.Website.Services;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,5 +18,24 @@ public static class IgnisWebsiteExtensions
         services.AddScoped<IPageService, PageService>();
 
         return services;
+    }
+
+    public static string SanitizeTypeName(this TypeInfo type, bool withNamespace = false)
+    {
+        if (type == null) throw new ArgumentNullException(nameof(type));
+        
+        var name = withNamespace ? type.FullName! : type.Name;
+        // ReSharper disable once InvertIf
+        if (type.IsGenericType)
+        {
+            var index = name.LastIndexOf('`');
+            name = name[..index];
+
+            var args = type.GenericTypeParameters.Select(p => p.Name).ToList();
+            args.AddRange(type.GenericTypeArguments.Select(a => SanitizeTypeName(a.GetTypeInfo(), true)));
+            name += $"<{string.Join(", ", args)}>";
+        }
+
+        return name;
     }
 }
