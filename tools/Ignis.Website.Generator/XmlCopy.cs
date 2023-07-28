5fdc1784fa2bc756ec9fbdc3f7753f606640e6e3
dvolper <github@david.vollmers.org>
2023-07-28 23:29:55 +0200

    debug xml copy

--- tools/Ignis.Website.Generator/XmlCopy.cs
+++ tools/Ignis.Website.Generator/XmlCopy.cs
@@ -2,18 +2,25 @@
 
 internal class XmlCopy
 {
-    private XmlCopy() {}
-    
+    private XmlCopy() { }
+
     public static void Copy(DirectoryInfo sourceDirectory, DirectoryInfo outputDirectory)
     {
         var xmlFiles = sourceDirectory.EnumerateFiles("*.xml", SearchOption.AllDirectories);
 
         foreach (var xmlFile in xmlFiles)
         {
-            var relativePath = xmlFile.FullName.Replace(sourceDirectory.FullName, string.Empty).TrimStart('\\');
+            Console.WriteLine($"XML: {xmlFile.FullName}");
+            
+            var relativePath = xmlFile.FullName.Replace(sourceDirectory.FullName, string.Empty)
+                .TrimStart(Path.DirectorySeparatorChar);
 
+            Console.WriteLine($"RP: {relativePath}");
+            
             var destinationPath = Path.Combine(outputDirectory.FullName, relativePath);
 
+            Console.WriteLine($"DP: {destinationPath}");
+            
             var destinationDirectory = new FileInfo(destinationPath).Directory!;
 
             if (!destinationDirectory.Exists)
