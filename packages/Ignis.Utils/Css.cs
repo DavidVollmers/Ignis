a8acef780e7b1a7d6ab942d250e85da98b609b34
dvolper <github@david.vollmers.org>
2023-08-08 15:12:53 +0200

    allow nullable strings in Css.Class

--- packages/Ignis.Utils/Css.cs
+++ packages/Ignis.Utils/Css.cs
@@ -4,17 +4,17 @@ namespace Ignis.Utils;
 
 public static class Css
 {
-    public static string Class(params string[] classes)
+    public static string Class(params string?[] classes)
     {
         return string.Join(" ", classes).Trim();
     }
 
-    public static string Class(params (string, bool)[] classes)
+    public static string Class(params (string?, bool)[] classes)
     {
         return string.Join(" ", classes.Where(x => x.Item2).Select(x => x.Item1)).Trim();
     }
 
-    public static string Class(string always, params (string, bool)[] classes)
+    public static string Class(string? always, params (string?, bool)[] classes)
     {
         return string.Join(" ", classes.Where(x => x.Item2).Select(x => x.Item1).Prepend(always)).Trim();
     }
@@ -24,7 +24,7 @@ public static class Css
         return string.Join(" ", cssClass.Select(x => x.CssClass)).Trim();
     }
 
-    public static string Class(string always, params ICssClass[] cssClass)
+    public static string Class(string? always, params ICssClass[] cssClass)
     {
         return string.Join(" ", cssClass.Select(x => x.CssClass).Prepend(always)).Trim();
     }
