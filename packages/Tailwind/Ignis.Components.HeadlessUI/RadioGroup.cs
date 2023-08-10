6041262970e54c64f210bf5ecbe5505c10d4e8de
dvolper <github@david.vollmers.org>
2023-08-10 18:15:00 +0200

    radio group component

---
+++ packages/Tailwind/Ignis.Components.HeadlessUI/RadioGroup.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Ignis.Components.HeadlessUI;
+
+public sealed class RadioGroup<TValue> : IgnisRigidComponentBase,  IRadioGroup
+{
+    private readonly AttributeCollection _attributes;
+    
+    private Type? _asComponent;
+    private string? _asElement;
+
+    /// <inheritdoc />
+    [Parameter]
+    public string? AsElement
+    {
+        get => _asElement;
+        set
+        {
+            _asElement = value;
+            _asComponent = null;
+        }
+    }
+
+    /// <inheritdoc />
+    [Parameter]
+    public Type? AsComponent
+    {
+        get => _asComponent;
+        set
+        {
+            _asComponent = value;
+            _asElement = null;
+        }
+    }
+
+    /// <summary>
+    /// The checked value.
+    /// </summary>
+    [Parameter]
+    public TValue? Value { get; set; }
+
+    /// <summary>
+    /// Occurs when the checked value changes.
+    /// </summary>
+    [Parameter]
+    public EventCallback<TValue?> ValueChanged { get; set; }
+
+    /// <inheritdoc />
+    [Parameter]
+    public RenderFragment<IRadioGroup>? _ { get; set; }
+
+    [Parameter] public RenderFragment<IRadioGroup>? ChildContent { get; set; }
+
+    /// <summary>
+    /// Additional attributes to be applied to the radio group.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IEnumerable<KeyValuePair<string, object?>>? AdditionalAttributes
+    {
+        get => _attributes.AdditionalAttributes;
+        set => _attributes.AdditionalAttributes = value;
+    }
+
+    /// <inheritdoc />
+    public ElementReference? Element { get; set; }
+
+    /// <inheritdoc />
+    public object? Component { get; set; }
+
+    /// <inheritdoc />
+    public IEnumerable<KeyValuePair<string, object?>> Attributes => _attributes;
+
+    public RadioGroup()
+    {
+        AsElement = "div";
+        
+        _attributes = new AttributeCollection(new []
+        {
+            () => new KeyValuePair<string, object?>("role", "radiogroup")
+        });
+    }
+
+    /// <inheritdoc />
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenAs(0, this);
+        builder.AddMultipleAttributes(1, Attributes!);
+        // ReSharper disable once VariableHidesOuterVariable
+        builder.AddContentFor(2, this, builder =>
+        {
+            builder.OpenComponent<CascadingValue<IRadioGroup>>(3);
+            builder.AddAttribute(4, nameof(CascadingValue<IRadioGroup>.IsFixed), true);
+            builder.AddAttribute(5, nameof(CascadingValue<IRadioGroup>.Value), this);
+            builder.AddAttribute(6, nameof(CascadingValue<IRadioGroup>.ChildContent),
+                this.GetChildContent(ChildContent));
+
+            builder.CloseComponent();
+        });
+
+        builder.CloseAs(this);
+    }
+}
