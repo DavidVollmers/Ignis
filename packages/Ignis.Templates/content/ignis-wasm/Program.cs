f3c7c67cf151e737cdfae2b3ddc51e6dca3f42f6
dvolper <github@david.vollmers.org>
2023-09-18 11:02:10 +0200

    ignis-wasm template

---
+++ packages/Ignis.Templates/content/ignis-wasm/Program.cs
@@ -0,0 +1,17 @@
+﻿using Ignis.Components.Web;
+using Ignis.Components.WebAssembly;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddIgnis();
+builder.Services.AddIgnisWebAssembly();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var app = builder.Build();
+
+await app.RunAsync();
