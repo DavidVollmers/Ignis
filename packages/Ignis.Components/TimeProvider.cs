3d74ea6623b7f598df95b339dbc308005ecbdbf9
dvolper <github@david.vollmers.org>
2023-11-26 16:42:14 +0100

    Support .NET 8.0 (#41)
    * support .net8.0 (RC2)
    
    * use System.TimeProvider with .net8.0
    
    * update remaining packages to .net8.0
    
    * update pipelines
    
    * update dependencies
    
    * fixes

--- packages/Ignis.Components/TimeProvider.cs
+++ packages/Ignis.Components/TimeProvider.cs
@@ -1,7 +1,8 @@
 ﻿namespace Ignis.Components;
 
-//TODO switch to System.TimeProvider when it's available
+// Substitute for System.TimeProvider
 // https://learn.microsoft.com/en-us/dotnet/api/system.timeprovider?view=net-8.0
+#if !NET8_0
 internal abstract class TimeProvider
 {
     public virtual ITimer CreateTimer(TimerCallback callback, object? state, TimeSpan dueTime, TimeSpan period)
@@ -9,3 +10,4 @@ internal abstract class TimeProvider
         return new TimerImplementation(callback, state, dueTime, period);
     }
 }
+#endif
