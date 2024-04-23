50ee71730a92c1b907eb7dff03e20b94bd0c9ade
Panos <panos_ath@outlook.com>
2024-04-23 23:52:02 +0300

    add ScrollIntoView method in AriaPopupExtensions

--- packages/Tailwind/Ignis.Components.HeadlessUI/Aria/AriaPopupExtensions.cs
+++ packages/Tailwind/Ignis.Components.HeadlessUI/Aria/AriaPopupExtensions.cs
@@ -1,16 +1,18 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
 
 namespace Ignis.Components.HeadlessUI.Aria;
 
 internal static class AriaPopupExtensions
 {
-    public static void SetActiveDescendant(this IAriaPopup popup, IAriaDescendant descendant, bool isActive)
+    public static void SetActiveDescendant(this IAriaPopup popup, IAriaDescendant descendant, bool isActive, IJSRuntime? jSRuntime = null)
     {
         if (descendant == null) throw new ArgumentNullException(nameof(descendant));
 
         if (isActive)
         {
             popup.ActiveDescendant = descendant;
+            _ = ScrollIntoView(popup.GetId(descendant), jSRuntime).ConfigureAwait(false);
         }
         else if (popup.ActiveDescendant == descendant)
         {
@@ -18,7 +20,7 @@ internal static class AriaPopupExtensions
         }
     }
 
-    public static void OnKeyDown(this IAriaPopup popup, KeyboardEventArgs eventArgs)
+    public static void OnKeyDown(this IAriaPopup popup, KeyboardEventArgs eventArgs, IJSRuntime? jSRuntime = null)
     {
         var descendants = popup.Descendants.ToArray();
 
@@ -41,23 +43,34 @@ internal static class AriaPopupExtensions
                 break;
             case "ArrowUp" when popup.ActiveDescendant == null:
             case "ArrowDown" when popup.ActiveDescendant == null:
-                if (descendants.Any()) popup.SetActiveDescendant(descendants[0], isActive: true);
+                if (descendants.Any()) popup.SetActiveDescendant(descendants[0], isActive: true, jSRuntime);
                 else if (!popup.IsOpen) popup.Open();
                 break;
             case "ArrowDown":
                 {
                     var index = Array.IndexOf(descendants, popup.ActiveDescendant) + 1;
-                    if (index < descendants.Length) popup.SetActiveDescendant(descendants[index], isActive: true);
+                    if (index < descendants.Length) popup.SetActiveDescendant(descendants[index], isActive: true, jSRuntime);
                     else if (!popup.IsOpen) popup.Open();
                     break;
                 }
             case "ArrowUp":
                 {
                     var index = Array.IndexOf(descendants, popup.ActiveDescendant) - 1;
-                    if (index >= 0) popup.SetActiveDescendant(descendants[index], isActive: true);
+                    if (index >= 0) popup.SetActiveDescendant(descendants[index], isActive: true, jSRuntime);
                     else if (!popup.IsOpen) popup.Open();
                     break;
                 }
         }
+    }
+
+    private static object ScrollOptions { get; } = new { Behavior = "smooth", Block = "nearest" };
+
+    private static async Task ScrollIntoView(string? id, IJSRuntime? jSRuntime)
+    {
+        if (jSRuntime is null || id is null) return;
+
+        var element = await jSRuntime.InvokeAsync<IJSObjectReference>("document.getElementById", id).ConfigureAwait(false);
+        await element.InvokeVoidAsync("scrollIntoView", ScrollOptions).ConfigureAwait(false);
+        await element.DisposeAsync().ConfigureAwait(false);
     }
 }
