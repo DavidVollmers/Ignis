0dd9a2537c80debfeb3f32542858e3b77c373535
dvolper <github@david.vollmers.org>
2024-05-07 19:33:07 +0200

    refactor api documentation to use doki

--- website/Ignis.Website/Components/ApiDocumentation/TypeDocumentationBase.cs
+++ website/Ignis.Website/Components/ApiDocumentation/TypeDocumentationBase.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+﻿using Doki;
 using Ignis.Components;
 using Ignis.Website.Services;
 using Microsoft.AspNetCore.Components;
@@ -8,22 +8,58 @@ namespace Ignis.Website.Components.ApiDocumentation;
 
 public abstract class TypeDocumentationBase : IgnisAsyncComponentBase, IHandleAfterRender
 {
-    [Parameter, EditorRequired] public TypeInfo Type { get; set; } = null!;
+    [Parameter, EditorRequired] public Type Type { get; set; } = null!;
 
     [Inject] public IStaticFileService StaticFileService { get; set; } = null!;
 
     // ReSharper disable once InconsistentNaming
     [Inject] public IJSRuntime JSRuntime { get; set; } = null!;
 
+    [Inject] public NavigationManager Router { get; set; } = null!;
+
+    protected AssemblyDocumentation AssemblyDocumentation { get; private set; } = null!;
+
+    protected NamespaceDocumentation NamespaceDocumentation { get; private set; } = null!;
+
+    protected TypeDocumentation TypeDocumentation { get; private set; } = null!;
+
     protected override async Task OnInitializedAsync(CancellationToken cancellationToken)
     {
-        var path = $"/_api/{Type.Assembly.GetName().Name}.xml";
+        var path = $"/_api/{Type.Assembly.GetName().Name}.json";
 
-        var xml = await StaticFileService.GetFileContentAsXmlAsync(path, cancellationToken);
+        var assembly =
+            await StaticFileService.GetFileContentAsJsonAsync<AssemblyDocumentation>(path, cancellationToken);
+        if (assembly == null)
+        {
+            Router.NavigateTo("/");
+            return;
+        }
+
+        AssemblyDocumentation = assembly;
+
+        var @namespace = AssemblyDocumentation.Namespaces.FirstOrDefault(n =>
+            string.Equals(n.Name, Type.Namespace, StringComparison.Ordinal));
+        if (@namespace == null)
+        {
+            Router.NavigateTo("/");
+            return;
+        }
+
+        NamespaceDocumentation = @namespace;
+
+        var type = NamespaceDocumentation.Types.FirstOrDefault(t =>
+            string.Equals(t.FullName, Type.FullName, StringComparison.Ordinal));
+        if (type == null)
+        {
+            Router.NavigateTo("/");
+            return;
+        }
+
+        TypeDocumentation = type;
     }
 
     public async Task OnAfterRenderAsync()
     {
-        await JSRuntime.InvokeVoidAsync("OnPageLoad");
+        await JSRuntime.InvokeVoidAsync("OnPageLoad", cancellationToken: CancellationToken);
     }
 }
--- website/Ignis.Website/IgnisWebsiteExtensions.cs
+++ website/Ignis.Website/IgnisWebsiteExtensions.cs
@@ -1,15 +1,12 @@
-﻿using System.Reflection;
-using System.Text.RegularExpressions;
-using System.Web;
+﻿using System.Web;
 using Ignis.Components;
 using Ignis.Components.Web;
 using Ignis.Website.Services;
-using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ignis.Website;
 
-public static partial class IgnisWebsiteExtensions
+public static class IgnisWebsiteExtensions
 {
     public static IServiceCollection AddIgnisWebsite(this IServiceCollection services)
     {
@@ -23,68 +20,10 @@ public static partial class IgnisWebsiteExtensions
         return services;
     }
 
-    public static string SanitizeTypeName(this TypeInfo type, bool withNamespace = false)
+    public static string GetTypeDocumentationLink(this Type type)
     {
-        if (type == null) throw new ArgumentNullException(nameof(type));
+        ArgumentNullException.ThrowIfNull(type);
 
-        var name = withNamespace ? type.FullName! : type.Name;
-        // ReSharper disable once InvertIf
-        if (type.IsGenericType)
-        {
-            var index = name.LastIndexOf('`');
-            name = name[..index];
-
-            var args = type.GenericTypeParameters.Select(p => p.Name).ToList();
-            args.AddRange(type.GenericTypeArguments.Select(a => a.GetTypeInfo().SanitizeTypeName(true)));
-            name += $"<{string.Join(", ", args)}>";
-        }
-
-        return name;
+        return $"/api/{HttpUtility.UrlEncode(type.AssemblyQualifiedName)}/_";
     }
-
-    public static MarkupString ResolveCodeComment(this string comment)
-    {
-        if (comment == null) throw new ArgumentNullException(nameof(comment));
-
-        var regex = SeeReferenceRegex();
-
-        var matches = regex.Matches(comment);
-        foreach (Match match in matches)
-        {
-            var cref = match.Groups["cref"].Value;
-            var typeInfo = ResolveCRef(cref);
-            if (typeInfo == null) continue;
-
-            var name = typeInfo.SanitizeTypeName();
-            var link = typeInfo.GetTypeDocumentationLink();
-            comment = comment.Replace(match.Value, $"<a href=\"{link}\">{name}</a>");
-        }
-
-        return new MarkupString(comment);
-    }
-
-    private static TypeInfo? ResolveCRef(string cref)
-    {
-        if (cref == null) throw new ArgumentNullException(nameof(cref));
-
-        var type = Type.GetType(cref);
-        if (type != null) return type.GetTypeInfo();
-
-        var parts = cref.Split('.');
-        var ns = string.Join('.', parts[..^1]);
-        var assembly = Assembly.Load(ns);
-
-        type = assembly.GetType(cref);
-        return type?.GetTypeInfo();
-    }
-
-    public static string GetTypeDocumentationLink(this TypeInfo typeInfo)
-    {
-        if (typeInfo == null) throw new ArgumentNullException(nameof(typeInfo));
-
-        return $"/api/{HttpUtility.UrlEncode(typeInfo.AssemblyQualifiedName)}/_";
-    }
-
-    [GeneratedRegex("<see cref=\"T:(?<cref>.+?)\"\\s*/>", RegexOptions.Compiled)]
-    private static partial Regex SeeReferenceRegex();
 }
